7899c44c46178e14c0a4e2327d77e32c58d2e826
Martin Giger <martin@humanoids.be>
2022-08-24 16:07:13 +0200

    Remove need for GStaticCorsFaker

--- src/background/background.js
+++ src/background/background.js
@@ -1,7 +1,5 @@
 "use strict";
 
-import * as GStaticCorsFaker from "./modules/GStaticCorsFaker.js";
 import * as SoundModule from "./modules/SoundModule.js";
 
-GStaticCorsFaker.init();
 SoundModule.init();
